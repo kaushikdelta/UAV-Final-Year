ec18a873f5338d31f9e96da044a77227a7a32498
Kaushik Dutta <42523211+kaushikdelta@users.noreply.github.com>
2019-04-15 19:29:26 +0530

    Kamikaze delta...win 10
    code.py for the main raspberry pi control and automation code.
    main.py is the program to start the whole program.
    multiwii.py is the code for navigation using the Arducopter.
    server.py is the server program to connect via MAV Link i guess.
    followface.py is an extra program for camera module, not necessary here.

---
+++ followface.py
@@ -0,0 +1,49 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (320, 240)
+camera.framerate = 8.0
+rawCapture = PiRGBArray(camera, size=(320, 240))
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 8, (320,240))
+
+# allow the camera to warmup
+time.sleep(0.1)
+
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
+
+    #Load a cascade file for detecting faces
+    face_cascade = cv2.CascadeClassifier('/home/pi/project/faces.xml')
+
+    #convert to grayscale
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+    #Look for faces in the image using the loaded cascade file
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    
+    #Draw a rectangle around every found face
+    for (x,y,w,h) in faces:
+        cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,0),2)
+    
+    # show the frame
+    out.write(image)
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
